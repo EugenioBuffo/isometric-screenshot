3924410588e969310ea1834f37be11192238316d
Eugenio B <buffo.eugenio@gmail.com>
2025-08-31 16:08:35 +0200

    Initial mod configuration

---
+++ src/main/java/it/eugeniobuffo/isometric/IsometricScreenshotClient.java
@@ -0,0 +1,11 @@
+package it.eugeniobuffo.isometric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IsometricScreenshotClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        // Client-side initialization logic
+    }
+}
